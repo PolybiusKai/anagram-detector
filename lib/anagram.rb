fce98885068b6db80d407ab00e0c0ee3645414d1
rsmith <legendary.belief@gmail.com>
2021-03-10 15:00:38 -0800

    commit tree 8d0a1f9dd14d9cc7ffd373cf3c4ca72d908a29e2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,35 @@
 # Your code goes here!
+#This is a TTD; you can use learn/rspec --fail-fast
+class Anagram
+    attr_accessor :words
+    
+    def initialize(words)
+      @words = words
+    end
+
+    def match(array) 
+        #Should take an array of possible anagrams
+        # Should return all matches in an array.
+        #If no matches exist, it should return an []
+
+        #You'll need to iterate over the array of words that 
+        # the .match method takes as an argument. You will 
+        # compare each word of that array to the word that the Anagram 
+        # class is initialized with.
+
+        #To determine if they are anagrams, try determining if they are 
+        # composed of the same letters. Remember that you can split a word 
+        # into an array of letters using some_word.split(""). You can compare 
+        # two arrays using the ==.
+        #Remember that you can .sort an arrays elements
+        array.select { |letters| 
+			letters.split("").sort.join == @words.split("").sort.join
+        }
+
+    end
+  
+end
+
+
+#listen = Anagram.new("listen")
+#listen.match(%w({Enter some words here}))
