0ab6072ae5a36be15915e3deaec527873df93843
rsmith <legendary.belief@gmail.com>
2021-03-10 16:53:05 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+#This is a TTD; you can use learn/rspec --fail-fast
+class Anagram
+    attr_accessor :words
+    
+    def initialize(words)
+      @words = words
+    end
+
+    def match(array) 
+
+        array.select { |letters| 
+			letters.split("").sort.join == @words.split("").sort.join
+        }
+
+    end
+  
+end
+
+
+#listen = Anagram.new("listen")
+#listen.match(%w({Enter some words here}))
